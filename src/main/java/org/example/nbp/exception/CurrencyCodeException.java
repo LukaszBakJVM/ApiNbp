cf75b0a6265dcc9f78f5b1fcfd097593ece58ab9
Łukasz Bąk <bbzzyyczczeek@interia.pl>
2024-10-23 09:22:10 +0200

    CurrencyCodeException

---
+++ src/main/java/org/example/nbp/exception/CurrencyCodeException.java
@@ -0,0 +1,7 @@
+package org.example.nbp.exception;
+
+public class CurrencyCodeException extends RuntimeException{
+    public CurrencyCodeException(String message) {
+        super(message);
+    }
+}
